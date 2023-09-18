4e62355812974051284bec154dd0780a12c654d0
Fabio Muriel <fabiomurielvega16@gmail.com>
2023-09-18 00:24:48 -0500

    Bug

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:libro_verse/src/UI/screens/screens.dart';
+
+class AppRouter{
+
+  static const String loginRoute = '/login';
+  static const String onBoardingRoute = '/onboarding';
+
+  static Route<dynamic> generateRoute(RouteSettings settings){
+    switch(settings.name){
+      case onBoardingRoute:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case loginRoute:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());;
+      default:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+    }
+  }
+
+}
