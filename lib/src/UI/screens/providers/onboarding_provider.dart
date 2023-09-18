22ed54f1bc44db1646448626155e6af089409dc6
Victor Manuel Lagunas Franco <victormanuelfrancodev@gmail.com>
2023-09-18 15:45:21 +0900

    left the pagecontroller to the top classes

--- lib/src/UI/screens/providers/onboarding_provider.dart
+++ lib/src/UI/screens/providers/onboarding_provider.dart
@@ -2,19 +2,17 @@ import 'package:flutter/material.dart';
 
 class OnboardingProvider with ChangeNotifier {
   int _currentPage = 0;
-  final PageController _pageController = PageController();
 
   int get getCurrentPage => _currentPage;
-  PageController get pageController => _pageController;
 
   void updateCurrentPage(int page) {
+    if (page < 0 || page > 2) return;
     _currentPage = page;
     notifyListeners();
   }
 
   @override
   void dispose() {
-    _pageController.dispose();
     super.dispose();
   }
 }
--- lib/src/UI/screens/start/onboarding_screen.dart
+++ lib/src/UI/screens/start/onboarding_screen.dart
@@ -4,92 +4,109 @@ import 'package:libro_verse/src/UI/screens/start/views/page_content.dart';
 import 'package:provider/provider.dart';
 import '../../../../utils/constants/onboarding_texts.dart';
 
-class OnBoardingScreen extends StatelessWidget {
-   
+class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  late PageController _pageController = PageController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _pageController = PageController();
+  }
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
-    var onBoardingProvider = Provider.of<OnboardingProvider>(context);
-
     return Scaffold(
-      body: Stack(
-        children: [
-          _OnBoardingPages(onBoardingProvider: onBoardingProvider),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: _Dots(size: size),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: EdgeInsets.only(bottom: size.height * 0.03),
-              width: 200,
-              height: 65,
-              child: const _NextButton(),
-            ),
-          )
-        ],
-      )
-    );
-  }
-}
-
-class _NextButton extends StatelessWidget {
-  const _NextButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    var onBoardingProvider = Provider.of<OnboardingProvider>(context);
-
-    return ElevatedButton(
-      onPressed: (){
-         onBoardingProvider.updateCurrentPage(onBoardingProvider.getCurrentPage + 1);
-      },
-      style: ButtonStyle(
-        elevation: MaterialStateProperty.all(5),
-        backgroundColor: MaterialStateProperty.all(const Color(0xffA8A6FF)),
-        shape: MaterialStateProperty.all(const StadiumBorder())
-      ),
-      child: const Text('Next', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),)
-    );
-  }
-}
-
-class _OnBoardingPages extends StatelessWidget {
-  const _OnBoardingPages({
-    required this.onBoardingProvider,
-  });
-
-   final OnboardingProvider onBoardingProvider;
-  @override
-  Widget build(BuildContext context) {
-    return PageView(
-      controller: onBoardingProvider.pageController,
-      onPageChanged: (int index) {
-         onBoardingProvider.updateCurrentPage(index);
-      } ,
+        body: Stack(
       children: [
-        PageViewContent(
-          titulo: OnBoardingText.title1,
-          subtitulo: OnBoardingText.subtitle1,
-          imagen: OnBoardingText.img1
+        OnBoardingPages(pageController: _pageController,),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: _Dots(size: size),
         ),
-        PageViewContent(
-          titulo: OnBoardingText.title2,
-          subtitulo: OnBoardingText.subtitle2,
-          imagen: OnBoardingText.img2
-        ),
-        PageViewContent(
-          titulo: OnBoardingText.title3,
-          subtitulo: OnBoardingText.subtitle3,
-          imagen: OnBoardingText.img3
-        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin: EdgeInsets.only(bottom: size.height * 0.03),
+            width: 200,
+            height: 65,
+            child: NextButton(pageController: _pageController,),
+          ),
+        )
       ],
+    ));
+  }
+}
+
+class NextButton extends StatelessWidget {
+   NextButton({
+    required this.pageController,
+  });
+
+  PageController pageController;
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<OnboardingProvider>(
+  builder: (context, onBoardingProvider,children) {
+    return ElevatedButton(
+        onPressed: () {
+          pageController.animateToPage(onBoardingProvider.getCurrentPage + 1, duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+          onBoardingProvider.updateCurrentPage(onBoardingProvider.getCurrentPage + 1);
+        },
+        style: ButtonStyle(
+            elevation: MaterialStateProperty.all(5),
+            backgroundColor: MaterialStateProperty.all(const Color(0xffA8A6FF)),
+            shape: MaterialStateProperty.all(const StadiumBorder())),
+        child: const Text(
+          'Next',
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black),
+        ));
+  },
+);
+  }
+}
+
+
+
+class OnBoardingPages extends StatelessWidget {
+
+  OnBoardingPages({required this.pageController});
+
+  PageController pageController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<OnboardingProvider>(
+      builder: (context, onBoardingProvider, child) {
+        return PageView(
+          controller: pageController,
+          onPageChanged: (int index) {
+            Provider.of<OnboardingProvider>(context, listen: false)
+                .updateCurrentPage(index);
+          },
+          children: [
+            PageViewContent(
+                titulo: OnBoardingText.title1,
+                subtitulo: OnBoardingText.subtitle1,
+                imagen: OnBoardingText.img1),
+            PageViewContent(
+                titulo: OnBoardingText.title2,
+                subtitulo: OnBoardingText.subtitle2,
+                imagen: OnBoardingText.img2),
+            PageViewContent(
+                titulo: OnBoardingText.title3,
+                subtitulo: OnBoardingText.subtitle3,
+                imagen: OnBoardingText.img3),
+          ],
+        );
+      },
     );
   }
 }
@@ -104,26 +121,34 @@ class _Dots extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var onBoardingProvider = Provider.of<OnboardingProvider>(context);
+    return Consumer<OnboardingProvider>(
+  builder: (context, onBoardingProvider, children) {
     return Container(
       margin: EdgeInsets.only(bottom: size.height * 0.15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           _Dot(active: onBoardingProvider.getCurrentPage == 0),
-          const SizedBox(width: 10,),
+          const SizedBox(
+            width: 10,
+          ),
           _Dot(active: onBoardingProvider.getCurrentPage == 1),
-          const SizedBox(width: 10,),
+          const SizedBox(
+            width: 10,
+          ),
           _Dot(active: onBoardingProvider.getCurrentPage == 2),
         ],
       ),
     );
+  },
+);
   }
 }
 
 class _Dot extends StatelessWidget {
   const _Dot({
-    super.key, required this.active,
+    super.key,
+    required this.active,
   });
 
   final bool active;
@@ -134,9 +159,10 @@ class _Dot extends StatelessWidget {
       width: 11,
       height: 11,
       decoration: BoxDecoration(
-        color: active ? const Color(0xffA8A6FF) : const Color.fromARGB(255, 187, 187, 187),
-        borderRadius: BorderRadius.circular(5)
-      ),
+          color: active
+              ? const Color(0xffA8A6FF)
+              : const Color.fromARGB(255, 187, 187, 187),
+          borderRadius: BorderRadius.circular(5)),
     );
   }
 }
